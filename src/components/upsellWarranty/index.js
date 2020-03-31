76ca69ca1f0784982f40a7d2fad53b54a3b8a5b4
bich.ngo <bich.ngo@dfo.global>
2020-03-31 10:43:04 +0700

    [CTR-209] checkout flow

--- src/components/upsellWarranty/index.js
+++
@@ -1,7 +0,0 @@
-import './style.scss'
-import './script'
-export {default as template} from './index.html'
-
-// const template = import('./index.html')
-// console.log(template)
-// console.log('importimportimportimport')
