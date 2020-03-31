fddaa719dbcd1be378ed0d65e6a90635495da6ec
bich.ngo <bich.ngo@dfo.global>
2020-03-31 10:06:44 +0700

    [CTR-209] checkout flow

---
+++ src/components/sample/index.js
@@ -0,0 +1,7 @@
+import './style.scss'
+import './script'
+export {default as template} from './index.html'
+
+// const template = import('./index.html')
+// console.log(template)
+// console.log('importimportimportimport')
